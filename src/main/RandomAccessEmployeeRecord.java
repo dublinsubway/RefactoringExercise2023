46e9e3ec1dc321abcc7f28292747ffa8f3d841e2
dublinsubway <61555192+dublinsubway@users.noreply.github.com>
2023-03-05 01:38:47 +0000

    Stylistic changes

--- src/main/RandomAccessEmployeeRecord.java
+++ src/main/RandomAccessEmployeeRecord.java
@@ -41,12 +41,13 @@ public class RandomAccessEmployeeRecord extends Employee
    // Ensure that string is correct length
    private String readName( RandomAccessFile file ) throws IOException
    {
-      char name[] = new char[ 20 ], temp;
+      char[] name = new char[20];
+      char temp;
 
       for ( int count = 0; count < name.length; count++ )
       {
          temp = file.readChar();
-         name[ count ] = temp;
+         name[count] = temp;
       } // end for     
       
       return new String( name ).replace( '\0', ' ' );
